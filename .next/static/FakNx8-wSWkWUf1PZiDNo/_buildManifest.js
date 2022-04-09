222b8f7e3f00f7566d539e59ce74c2b291031b1e
Matthew Wang <wmatthew123@gmail.com>
2022-04-09 17:52:07 +0800

    git configs

---
+++ .next/static/FakNx8-wSWkWUf1PZiDNo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/751-84f38fc8c1fa3a3e.js","static/chunks/pages/index-a5b441115f671573.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
