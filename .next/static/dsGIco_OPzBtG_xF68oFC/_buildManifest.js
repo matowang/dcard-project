98409bf49077813c2f9fd327d19e8e678e362ffe
Matthew Wang <wmatthew123@gmail.com>
2022-04-10 14:19:27 +0800

    responsive header

--- .next/static/dsGIco_OPzBtG_xF68oFC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/315-8eafcbe07c8a33ba.js","static/chunks/pages/index-850b5878aefeed9e.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
